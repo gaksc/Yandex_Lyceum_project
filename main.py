245bf09c84ba425c063d4126c532df929596a4b9
Nikita <75705735+gaksc@users.noreply.github.com>
2023-04-27 10:23:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from PIL import Image, ImageGrab
 import time
 import webbrowser
 import sqlite3
+
 action = ''
 con = sqlite3.connect("users.db", check_same_thread=False)
 cur = con.cursor()
@@ -18,11 +19,13 @@ idk = cur.execute("""SELECT id FROM users""").fetchall()
 ids = []
 rights = open('authorized.txt', 'a+')
 rights.flush()
-authorized = open("authorized.txt","r").readlines()
+authorized = open("authorized.txt", "r").readlines()
 authorized = [i.strip() for i in authorized]
 your_id = 1
 for i in idk:
     ids.append(i[0])
+
+
 def check(message):
     global ids, idk
     if message.chat.id not in ids:
@@ -40,6 +43,7 @@ def check(message):
 
 bot_token = BOT_TOKEN
 bot = telebot.TeleBot(bot_token)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -75,16 +79,19 @@ def send_welcome(message):
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
 
+
 @bot.message_handler(commands=['help'])
 def send_help(message):
     try:
         check(message)
-        bot.reply_to(message, "Commands: \n/shutdown - shutdown the computer \n/restart - restart the computer \n/sleep - put the computer to sleep \n /info - information about PC"+
-                              "\n/screenshot - Make a screenshot \n/who - Who used this bot \n/fish - Use when fish \n/authorize - Give rights to restart, sleep and shutdown " +
+        bot.reply_to(message,
+                     "Commands: \n/shutdown - shutdown the computer \n/restart - restart the computer \n/sleep - put the computer to sleep \n /info - information about PC" +
+                     "\n/screenshot - Make a screenshot \n/who - Who used this bot \n/fish - Use when fish \n/authorize - Give rights to restart, sleep and shutdown " +
                      "\n/unauthorize - Downgrade rights of users")
     except Exception as e:
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
+
 
 @bot.message_handler(commands=['shutdown'])
 def shutdown_computer(message):
@@ -99,6 +106,7 @@ def shutdown_computer(message):
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
 
+
 @bot.message_handler(commands=['restart'])
 def restart_computer(message):
     try:
@@ -112,6 +120,7 @@ def restart_computer(message):
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
 
+
 @bot.message_handler(commands=['sleep'])
 def sleep_computer(message):
     try:
@@ -124,6 +133,7 @@ def sleep_computer(message):
     except Exception as e:
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
+
 
 @bot.message_handler(commands=['info'])
 def sleep_computers(message):
@@ -156,11 +166,14 @@ def sleep_computers(message):
             user = os.getlogin()
         except Exception:
             user = 'Pycharm'
-        bot.send_photo(message.chat.id, img, caption=result['country_name'] + ', ' + result['state'] + ', ' + result['city']
-                       + '\n' + 'PC: ' + user + '\nOS: ' + windows + '\nProcessor: ' + processor)
+        bot.send_photo(message.chat.id, img,
+                       caption=result['country_name'] + ', ' + result['state'] + ', ' + result[
+                           'city']
+                               + '\n' + 'PC: ' + user + '\nOS: ' + windows + '\nProcessor: ' + processor)
     except Exception as e:
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
+
 
 @bot.message_handler(commands=['screenshot'])
 def screenshot_computer(message):
@@ -173,6 +186,7 @@ def screenshot_computer(message):
     except Exception as e:
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
+
 
 @bot.message_handler(commands=['who'])
 def who(message):
@@ -187,6 +201,7 @@ def who(message):
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
 
+
 @bot.message_handler(commands=['fish'])
 def fish(message):
     try:
@@ -198,6 +213,7 @@ def fish(message):
         time.sleep(2)
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
 
+
 @bot.message_handler(commands=['authorize'])
 def giverights(message):
     try:
@@ -208,7 +224,8 @@ def giverights(message):
             markup = types.InlineKeyboardMarkup()
             result = cur.execute("""SELECT * FROM users""").fetchall()
             for i in result:
-                button1 = types.InlineKeyboardButton(str(i[1]) + ' ' + str(i[2]), callback_data=str(i[0]))
+                button1 = types.InlineKeyboardButton(str(i[1]) + ' ' + str(i[2]),
+                                                     callback_data=str(i[0]))
                 markup.add(button1)
             bot.send_message(message.chat.id, 'Кому выдать права?', reply_markup=markup)
             your_id = message.chat.id
@@ -231,7 +248,8 @@ def notgiverights(message):
             markup = types.InlineKeyboardMarkup()
             result = cur.execute("""SELECT * FROM users""").fetchall()
             for i in result:
-                button1 = types.InlineKeyboardButton(str(i[1]) + ' ' + str(i[2]), callback_data=str(i[0]))
+                button1 = types.InlineKeyboardButton(str(i[1]) + ' ' + str(i[2]),
+                                                     callback_data=str(i[0]))
                 markup.add(button1)
             bot.send_message(message.chat.id, 'У кого отнять права?', reply_markup=markup)
             your_id = message.chat.id
@@ -244,7 +262,7 @@ def notgiverights(message):
         bot.send_message(message.chat.id, 'ERROR ' + str(e))
 
 
-@bot.callback_query_handler(func=lambda call:True)
+@bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     global your_id, authorized, rights, action
     req = call.data.split('_')
@@ -268,4 +286,6 @@ def callback_query(call):
             bot.send_message(your_id, 'Успешно!')
         else:
             bot.send_message(your_id, 'Упс, id ' + str(idf) + ' не имеет админ прав')
+
+
 bot.polling()
